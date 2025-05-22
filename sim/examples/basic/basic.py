9bc168073192391ebfd97e98d24cc8ad5fc719b0
nacnud04 <duncanbyrne03@gmail.com>
2025-05-22 15:00:33 -0600

    Basic point target sim working and complete

--- sim/examples/basic/basic.py
+++ sim/examples/basic/basic.py
@@ -33,14 +33,15 @@ Cs = (1000, 1000, 25000)  # source location (x, y, z) [m]
 
 source = so.Source(dt, T, Cs)
 source.chirp(f0, B, correlate=False)
-#source.plot()
+source.plot()
 
 
 # --- STEP 3: Set up model ---
 
-Ct = (1000, 1250, -300)  # target location (x, y, z) [m]
+Ct = (1000, 1250, -10e3)  # target location (x, y, z) [m]
 
 model = si.Model(surf, source, vec=True)        # init model
+print(f"Range bins: {model.rb}")
 model.set_target(Ct)                  # set target location
 signals = []; compare = []
 for i in range(2):
@@ -49,7 +50,7 @@ for i in range(2):
     st = Time()
     model.gen_raypaths(progress_bar=True) # generate raypaths from source->facet->target
     #model.plot_s2f_angle()                # plot angles of incidence on surface
-    #model.plot_s2f_rad()                  # plot reradiation maintaned due to angle change by facet
+    #   model.plot_s2f_rad()                  # plot reradiation maintaned due to angle change by facet
 
 
     # --- STEP 4: Simulate ---
@@ -58,7 +59,7 @@ for i in range(2):
     print(f"Took {round(Time()-st, 2)} s")
 
     # generate synthetic signal
-    if model.vec: model.gen_timeseries_vec(time=True)
+    if model.vec: model.gen_timeseries_vec(time=True, doppler=False)
     else: model.gen_timeseries(time=True)
 
     signals.append(model.signal)
