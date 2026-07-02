2c80360cea18bee6e4dec431eaaad46527c12582
Duncan Byrne <duby2704@colorado.edu>
2026-07-02 12:58:41 -0600

    Testing subsurface offset target

---
+++ examples/UNIT_TESTS/subsurface_offset_target/code/output.py
@@ -0,0 +1,164 @@
+import sys
+import numpy as np
+from scipy.optimize import curve_fit
+import matplotlib.pyplot as plt
+
+sys.path.append("../../../src/PYTHON")
+import output_handling as oh
+import unit_convs      as uc
+
+h = np.linspace(19e3, 300e3, 250)
+
+def sinc(x):
+
+    mask = x != 0
+    result = np.zeros_like(x)
+    result[mask] = np.sin(x[mask] * np.pi) / (x[mask] * np.pi)
+    result[~mask] = 1
+    return result
+
+def facet_gain(th, ph, params):
+
+    fs = params["fs"]
+    lam = params["lam"]
+
+    piLlam = (np.pi * fs) / lam
+
+    sinc1 = sinc(piLlam * np.sin(th) * np.cos(ph))
+    sinc2 = sinc(piLlam * np.sin(th) * np.sin(ph))
+
+    D_norm = (sinc1 * sinc2) ** 2
+
+    D_max = (4 * np.pi * fs**2) / lam**2
+
+    return D_norm * D_max
+
+def facet_gain_wrap(h, d, xoff, params):
+
+    if xoff == 0:
+        th = np.zeros_like(h)
+    else:
+        th = np.arctan(xoff /(h + d))
+    ph = 0
+    return facet_gain(th, ph, params)
+
+def get_analytic(params, h, d, xoff):
+
+    lam = params["lam"]
+    G   = 10 ** (params["surface_gain"] / 10)
+    P_t = params["power"]
+    n   = np.sqrt(params["eps_2"])
+
+    # refraction gain
+    g_r = (h + d) / (h + d/n)
+
+    # target gain
+    G_T = facet_gain_wrap(h, d, xoff, params)
+
+    R = np.sqrt((h + d)**2 + xoff**2)
+
+    P_r = (P_t * G**2 * lam**4 * G_T**2) / ((4 * np.pi)**4 * (R)**4)
+    P_r *= g_r**4 / n**2
+
+    return P_r
+
+def get_target_phase(params, h, d, xoff):
+
+    #rng = h + np.sqrt(params["eps_2"]) * d
+    rng = h + d
+    rng = np.sqrt(rng**2 + xoff**2)
+    subsurf_ratio = d / (h + d)
+    corrected_rng = rng * (1 - subsurf_ratio) + rng * subsurf_ratio * np.sqrt(params["eps_2"])
+    cmplx = np.exp(((-4j * np.pi) / params['lam']) * corrected_rng)
+    return np.degrees(np.angle(cmplx))
+
+plt.rcParams.update({'font.size': 14})
+
+xoff = 2000
+
+depths = (5000, 1000, 500, 250)
+colors = ["red", "green", "purple", "blue"]
+contrast = ["maroon", "lime", "fuchsia", "cyan"]
+
+for d, c, cont in zip(depths, colors, contrast):
+
+    params = oh.load_params(f"inputs/params_{d:04d}.pkl", "inputs/layer.txt")
+
+    if d == depths[0]:
+        G_T_tmp = facet_gain_wrap(19e3, 250, 500, params)
+        print(f"G_T at 19 km altitude and 250 m depth with {xoff} m x-offset: {G_T_tmp:.6f}")
+        G_T_max = facet_gain_wrap(19e3, 250, 0, params)
+        print(f"Which is {G_T_tmp / G_T_max:.6f} times the max")
+
+    P_r = get_analytic(params, h, d, xoff)
+
+    # load radargram
+    rdrgrm = oh.compile_rdrgrm(f"rdrgrm/{d:04d}", params)
+
+    #plt.imshow(np.abs(rdrgrm), aspect="auto")
+    #plt.show()
+
+    P_num = np.max(np.abs(rdrgrm)**2, axis=0)
+
+    # find the phase of the maximum signal for each trace
+    argmax = np.argmax(np.abs(rdrgrm), axis=0)
+    phse   = np.degrees(np.angle(rdrgrm[argmax, range(rdrgrm.shape[1])]))
+
+    error = np.abs(P_num - P_r) / P_r * 100
+
+    fig, ax = plt.subplots(2, figsize=(8, 5), sharex=True, gridspec_kw={'height_ratios': [4, 1]})
+
+    ax[0].plot(h/1e3, uc.lin_to_db(P_num), label=f"Numerical", color=c, linewidth=1)
+    ax[0].plot(h/1e3, uc.lin_to_db(P_r), color=cont, linestyle="--", label=f"Analytic", linewidth=1)
+
+    ax[1].plot(h/1e3, error, color=c, label=f"Error (%): d={d} m", linewidth=1)
+
+    ax[0].set_ylabel("Maximum Power [dBW]")
+    ax[0].legend()
+    ax[0].set_title(f"Off-nadir Subsurface Facet - Depth = {d} m")
+
+    ax[1].set_xlabel("Altitude [km]")
+    ax[1].set_ylabel("Error (%)")
+    #ax[1].set_ylim(0, 10)
+
+    plt.xlim(h[-1]/1e3, h[0]/1e3)
+    plt.savefig(f"figures/SubsurfaceFacet{d:04d}.png", dpi=300)
+
+    plt.close()
+
+#fig, ax = plt.subplots(figsize=(8, 5))
+fig, ax = plt.subplots(figsize=(10, 8))
+
+ax.set_xlim(h[-1]/1e3, h[0]/1e3)
+
+for d, c, cont in zip(depths, colors, contrast):
+
+    ana_phse = get_target_phase(params, h, d, xoff)
+
+    # load radargram
+    rdrgrm = oh.compile_rdrgrm(f"rdrgrm/{d:04d}", params)
+
+    # find the phase of the maximum signal for each trace
+    argmax = np.argmax(np.abs(rdrgrm), axis=0)
+    phse   = np.degrees(np.angle(rdrgrm[argmax, range(rdrgrm.shape[1])]))
+
+    phase_error = phse - ana_phse
+
+    # make sure phase error is between -180 and 180
+    phase_error = (phase_error + 180) % 360 - 180
+
+    # mean phase error
+    mean_error = np.mean(phase_error)
+
+    # line showing mean error with text at end
+    ax.axhline(mean_error, color=c, alpha=0.5, linestyle="--")
+    ax.text(h[0]/1e3 + 1, mean_error, f"{mean_error:.2f}\ndeg", color=c, alpha=0.7, fontsize=12, verticalalignment="center")
+
+    ax.plot(h/1e3, phase_error, label=f"Target depth: {d} m", color=c)
+
+ax.set_ylabel("Phase Error (deg)")
+ax.set_xlabel("Altitude [km]")
+ax.legend(fontsize=10)
+ax.set_title("Phase Error of Subsurface Facet")
+plt.savefig("figures/SubsurfaceFacet_PhaseError.png", dpi=300)
+plt.show()
---
+++ examples/UNIT_TESTS/subsurface_offset_target/code/params.py
@@ -0,0 +1,68 @@
+import sys
+import numpy as np
+
+sys.path.append("../../../src/PYTHON")
+import simple_surfaces as ss
+import param_gen       as pg
+
+domainpar = {
+    "ox": -1e3,
+    "oy": -1e3,
+    "oz": 0,
+    "fs": 2.5,
+    "nx": 800,
+    "ny": 800,
+}
+
+"""
+domainpar = {
+    "ox": 80,
+    "oy": -20,
+    "oz": 0,
+    "fs": 1,
+    "nx": 40,
+    "ny": 40,
+}
+"""
+recpar = {
+    "rx_window_m": 1.25e3,         # receive window size [m]
+    "rx_sample_rate": 60e6,       # receive sample rate [Hz]
+    "rx_window_position_file": "inputs/rx_window_positions.txt",
+}
+
+sourcepar = {
+    "ns": 250,               # source count            [.]
+    "source_path_file": "inputs/source_path.txt",
+    "aperture": 70,
+}
+
+otherpar = {
+    "lossless": True,
+    "debug_surface": False,
+    "disable_surface": True,
+}
+
+tdepths = (-5e3, -2.5e3, -1e3, -0.5e3, -0.25e3)
+
+# first export basic single example stuff
+params = pg.gen_params("REASON_VHF", "planetary_ice", domainpar, recpar, sourcepar, par=otherpar)
+for depth in tdepths:
+    params["rx_window_position_file"] = f"inputs/rx_window_positions_{abs(int(depth)):04d}.txt"
+    pg.export_params(params, f"params_{abs(int(depth)):04d}")
+
+ss.make_surface(params, "flat", f"inputs/facets.fct")
+#ss.make_target_array(params, "flat", f"inputs/layer.txt", zoffset=-0.5e3)
+
+# --- MAKE SOURCE PATH ---
+
+maxZ = 300e3 # maximum altitude of source path [m]
+minZ = 19e3 # minimum altitude of source path [m]
+
+params['sz'] = maxZ
+Fr = ss.calc_fresnel(params)
+print(f"Fresnel zone radius for maximum altitude of {minZ} m is {Fr:.2f} m")
+sz = pg.vert_source_path(params, minZ, maxZ, "source_path", xoff=2000)
+
+# --- RX OPENING WINDOW FILE ---
+for depth in tdepths:
+    pg.track_Z_rxwin(sz, depth * np.sqrt(params['eps_2']) + 500, f"rx_window_positions_{abs(int(depth)):04d}")
